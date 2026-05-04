333ade032bd26056d61597760debd791a0c57235
Michael Vera <michaelvera13@gmail.com>
2026-05-04 11:10:28 -0500

    Set output option to 'static' in config

--- astro.config.ts
+++ astro.config.ts
@@ -8,6 +8,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: SITE.website,
   base: "/astro-blog",
   integrations: [
